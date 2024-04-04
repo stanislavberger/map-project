d0b5e495b8ac4cb10d7c2fa15820ee5a105f83c1
stanislavberger <berger.pro@yandex.ru>
2024-04-04 11:15:33 +0300

    add types for buttons

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,8 +1,12 @@
 import { Button }from 'antd';
 import { useState } from 'react';
+import type { ConfigProviderProps } from 'antd';
 
-const HeroButton: React.FC = (props) => {
+
+const HeroButton: React.FC = () => {
     
+    type SizeType = ConfigProviderProps['componentSize'];
+
     const [size, setSize] = useState<SizeType>('large');
 
     return (
